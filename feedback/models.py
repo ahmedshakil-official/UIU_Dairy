847783a4a62bdcb68f730f4821d24bfe0c4dd859
Farhana Khan <fkhan172191@bscse.uiu.ac.bd>
2021-12-07 19:05:45 +0600

    feedback models added

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Feedback(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    feedback = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
