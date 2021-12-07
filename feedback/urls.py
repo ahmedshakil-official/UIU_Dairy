328b4c4a258eaf1adc5d8e5d3397400bf4e3c0de
Farhana Khan <fkhan172191@bscse.uiu.ac.bd>
2021-12-07 19:52:36 +0600

    list view added

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'f'
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.IndexView.as_view(), name='home'),
+    path('detail/<pk>/', views.Details.as_view(), name='detail'),
+    path('add', views.Add.as_view(), name='add'),
+    path('update/<pk>', views.Update.as_view(), name='update'),
+    path('delete/<pk>', views.Delete.as_view(), name='delete'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from feedback import models
+
 
 # Create your views here.
+
+
+class IndexView(ListView):
+    context_object_name = 'feedback'
+    model = models.Feedback
+    template_name = 'feedback/index.html'
+
+
+
